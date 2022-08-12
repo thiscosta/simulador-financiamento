1580dfbd3424f69756b4d5aec22d8ffe33f9794b
Thiago Costa <37018497+thiscosta@users.noreply.github.com>
2022-08-12 08:34:57 -0300

    commit message

--- src/types.ts
+++ src/types.ts
@@ -17,9 +17,11 @@ export interface Card4Props {
   testprop1: any;
   testprop2: any;
   testprop6: any;
+  prop7: any;
 };
 
 export interface MeetingListItemProps {
   prop1: any;
   isHover: any;
+  teste: any;
 };
